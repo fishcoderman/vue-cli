5286d4d231d5e2b0c20468a7468c3f1169835a3c
chinamasters <1263641416@qq.com>
2020-06-02 09:50:24 +0800

    asdf

--- webpack.library.js
+++ webpack.library.js
@@ -2,7 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 /**
  * 库导出最好用 export default导出，因为import导入默认为： import library from 'library' => import {default as library} from 'library';
- * import需要default属性，如果是export导出，则改为 import {library} from 'library';
+ * 如果是export导出，则改为 import {library} from 'library';
  */
 module.exports = {
   mode: 'development',
